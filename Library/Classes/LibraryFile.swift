3d9a63828d165246a1b9d0aab788c940b2393f98
Vladyslav Yemets <vyemets@cogniance.com>
2018-12-24 19:00:19 +0200

    added test file

---
+++ Library/Classes/LibraryFile.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryFile.swift
+//  Library_Example
+//
+//  Created by Vladyslav Yemets on 12/24/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+public class SomeClass2 {
+    
+    func doSomething2() {
+        
+    }
+    
+}
---
+++ TestFile.swift
@@ -0,0 +1,8 @@
+//
+//  TestFile.swift
+//  Library
+//
+//  Created by Vladyslav Yemets on 12/24/18.
+//
+
+import Foundation
