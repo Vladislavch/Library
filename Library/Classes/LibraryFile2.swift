5c6c0b3ab89340cdf6139a73a460023f4bd31f2c
Vladyslav Yemets <vyemets@cogniance.com>
2018-12-24 19:07:21 +0200

    added test file

---
+++ Library/Classes/LibraryFile2.swift
@@ -0,0 +1,16 @@
+//
+//  LibraryFile2.swift
+//  Library
+//
+//  Created by Vladyslav Yemets on 12/24/18.
+//
+
+import Foundation
+
+public class SomeClass3 {
+    
+    func doSomething3() {
+        
+    }
+    
+}
